7bf40e1e91c64e82e742ac242291734a9feeab94
tgood111 <tgood111@live.com>
2017-10-02 12:45:40 -0500

    changed role to admin option

--- KnockoutsBoxing/Controllers/AdminController.cs
+++ KnockoutsBoxing/Controllers/AdminController.cs
@@ -25,7 +25,14 @@ namespace KnockoutsBoxing.Controllers
         //public async Task<ActionResult> UserDetails(string UserName)
         public async Task<ActionResult> UserDetails(string UserName)
         {
-           
+            /*
+             *             //This will collect the user with the sent user name
+            var tempuser = UserManager.Users.First(x => x.UserName.Equals(UserName));
+            //this will collect the user role 
+            var temp = await UserManager.GetRolesAsync(tempuser.Id);
+            ViewBag.ObtainedUserRole = temp[0];
+            return View(tempuser);
+             */
 
             //first I would like to get the User whom's role I want to see
             var CurrentUser = UserManager.Users.First(u => u.UserName.Equals(UserName));
@@ -42,6 +49,42 @@ namespace KnockoutsBoxing.Controllers
             return View();
 
 
+        }
+
+        //public async Task<ActionResult> UserUpgradeToCanSee(string UserName)
+        //ChangeToAdmin
+
+        public async Task<ActionResult> ChangeToAdmin(string UserName)
+        {
+            //first I would like to get the User whom's role I want to see
+            var CurrentUser = UserManager.Users.First(u => u.UserName.Equals(UserName));
+
+            //I want to collect the role he has. 
+            var CurrentUserRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+
+            if (CurrentUserRole.Count > 0)
+            {
+                ViewBag.OldUserRole = CurrentUserRole[0];
+                /*
+                 *             //remove existing role
+                    await UserManager.RemoveFromRoleAsync(tempuser.Id, temp[0]);
+                    var temp1 = await UserManager.GetRolesAsync(tempuser.Id);
+                 */
+                await UserManager.RemoveFromRoleAsync(CurrentUser.Id, CurrentUserRole[0]);
+            }
+            else
+            {
+                ViewBag.OldUserRole = "No Current Role Assigned";
+            }
+            //var result_of_role_change = await UserManager.AddToRoleAsync(tempuser.Id, "canSee");
+            var ResultRoleChange = await UserManager.AddToRoleAsync(CurrentUser.Id, "Administrator");
+
+            var NewRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+            ViewBag.NewUserRole = NewRole[0];
+
+            await db.SaveChangesAsync();
+
+            return View();
         }
 
         //This is that incredibly important UserManager that is used everywhere
