54157a8cb175894626b45a908c2ccc1dbfaf049c
tgood111 <tgood111@live.com>
2017-10-02 10:32:32 -0500

    added a view to show the user role

--- KnockoutsBoxing/Controllers/AdminController.cs
+++ KnockoutsBoxing/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
 ﻿using KnockoutsBoxing.Models;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -19,5 +21,49 @@ namespace KnockoutsBoxing.Controllers
 
             return View(listofallusers);
         }
+
+        //public async Task<ActionResult> UserDetails(string UserName)
+        public async Task<ActionResult> UserDetails(string UserName)
+        {
+           
+
+            //first I would like to get the User whom's role I want to see
+            var CurrentUser = UserManager.Users.First(u => u.UserName.Equals(UserName));
+
+
+            //I want to collect the role he has. 
+            var CurrentUserRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+
+            if(CurrentUserRole.Count>0)
+            {
+                ViewBag.UserRole = CurrentUserRole[0];
+            }
+
+            return View();
+
+
+        }
+
+        //This is that incredibly important UserManager that is used everywhere
+        //I copied this from the other controllers who are also using this method extensively
+        //to work on users that are extracted
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        //this is an object that allows you to use the UserManager
+        //UserManage is like a proxy for doing db operations on the user object
+        //its pretty userful
+        private ApplicationUserManager _userManager;
     }
+
+
 }
