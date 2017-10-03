33415d841126e45f6a4030f5529347fb7defb63f
tgood111 <tgood111@live.com>
2017-10-03 09:56:17 -0500

    added Fan, moderator, promotor, boxer roles

--- KnockoutsBoxing/Controllers/AdminController.cs
+++ KnockoutsBoxing/Controllers/AdminController.cs
@@ -85,7 +85,139 @@ namespace KnockoutsBoxing.Controllers
             await db.SaveChangesAsync();
 
             return View();
-        }
+        }//end of ChangeToAdmin
+
+        public async Task<ActionResult> ChangeToModerator(string UserName)
+        {
+            //first I would like to get the User whom's role I want to see
+            var CurrentUser = UserManager.Users.First(u => u.UserName.Equals(UserName));
+
+            //I want to collect the role he has. 
+            var CurrentUserRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+
+            if (CurrentUserRole.Count > 0)
+            {
+                ViewBag.OldUserRole = CurrentUserRole[0];
+                /*
+                 *             //remove existing role
+                    await UserManager.RemoveFromRoleAsync(tempuser.Id, temp[0]);
+                    var temp1 = await UserManager.GetRolesAsync(tempuser.Id);
+                 */
+                await UserManager.RemoveFromRoleAsync(CurrentUser.Id, CurrentUserRole[0]);
+            }
+            else
+            {
+                ViewBag.OldUserRole = "No Current Role Assigned";
+            }
+            //var result_of_role_change = await UserManager.AddToRoleAsync(tempuser.Id, "canSee");
+            var ResultRoleChange = await UserManager.AddToRoleAsync(CurrentUser.Id, "Moderator");
+
+            var NewRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+            ViewBag.NewUserRole = NewRole[0];
+
+            await db.SaveChangesAsync();
+
+            return View();
+        }//end of ChangeToModerator
+
+        public async Task<ActionResult> ChangeToPromotor(string UserName)
+        {
+            //first I would like to get the User whom's role I want to see
+            var CurrentUser = UserManager.Users.First(u => u.UserName.Equals(UserName));
+
+            //I want to collect the role he has. 
+            var CurrentUserRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+
+            if (CurrentUserRole.Count > 0)
+            {
+                ViewBag.OldUserRole = CurrentUserRole[0];
+                /*
+                 *             //remove existing role
+                    await UserManager.RemoveFromRoleAsync(tempuser.Id, temp[0]);
+                    var temp1 = await UserManager.GetRolesAsync(tempuser.Id);
+                 */
+                await UserManager.RemoveFromRoleAsync(CurrentUser.Id, CurrentUserRole[0]);
+            }
+            else
+            {
+                ViewBag.OldUserRole = "No Current Role Assigned";
+            }
+            //var result_of_role_change = await UserManager.AddToRoleAsync(tempuser.Id, "canSee");
+            var ResultRoleChange = await UserManager.AddToRoleAsync(CurrentUser.Id, "Promoter");
+
+            var NewRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+            ViewBag.NewUserRole = NewRole[0];
+
+            await db.SaveChangesAsync();
+
+            return View();
+        }//end of ChangeToPromotor
+
+        public async Task<ActionResult> ChangeToBoxer(string UserName)
+        {
+            //first I would like to get the User whom's role I want to see
+            var CurrentUser = UserManager.Users.First(u => u.UserName.Equals(UserName));
+
+            //I want to collect the role he has. 
+            var CurrentUserRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+
+            if (CurrentUserRole.Count > 0)
+            {
+                ViewBag.OldUserRole = CurrentUserRole[0];
+                /*
+                 *             //remove existing role
+                    await UserManager.RemoveFromRoleAsync(tempuser.Id, temp[0]);
+                    var temp1 = await UserManager.GetRolesAsync(tempuser.Id);
+                 */
+                await UserManager.RemoveFromRoleAsync(CurrentUser.Id, CurrentUserRole[0]);
+            }
+            else
+            {
+                ViewBag.OldUserRole = "No Current Role Assigned";
+            }
+            //var result_of_role_change = await UserManager.AddToRoleAsync(tempuser.Id, "canSee");
+            var ResultRoleChange = await UserManager.AddToRoleAsync(CurrentUser.Id, "Boxer");
+
+            var NewRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+            ViewBag.NewUserRole = NewRole[0];
+
+            await db.SaveChangesAsync();
+
+            return View();
+        }//end of ChangeToBoxer
+
+        public async Task<ActionResult> ChangeToFan(string UserName)
+        {
+            //first I would like to get the User whom's role I want to see
+            var CurrentUser = UserManager.Users.First(u => u.UserName.Equals(UserName));
+
+            //I want to collect the role he has. 
+            var CurrentUserRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+
+            if (CurrentUserRole.Count > 0)
+            {
+                ViewBag.OldUserRole = CurrentUserRole[0];
+                /*
+                 *             //remove existing role
+                    await UserManager.RemoveFromRoleAsync(tempuser.Id, temp[0]);
+                    var temp1 = await UserManager.GetRolesAsync(tempuser.Id);
+                 */
+                await UserManager.RemoveFromRoleAsync(CurrentUser.Id, CurrentUserRole[0]);
+            }
+            else
+            {
+                ViewBag.OldUserRole = "No Current Role Assigned";
+            }
+            //var result_of_role_change = await UserManager.AddToRoleAsync(tempuser.Id, "canSee");
+            var ResultRoleChange = await UserManager.AddToRoleAsync(CurrentUser.Id, "Fan");
+
+            var NewRole = await UserManager.GetRolesAsync(CurrentUser.Id);
+            ViewBag.NewUserRole = NewRole[0];
+
+            await db.SaveChangesAsync();
+
+            return View();
+        }//end of ChangeToFan
 
         //This is that incredibly important UserManager that is used everywhere
         //I copied this from the other controllers who are also using this method extensively
